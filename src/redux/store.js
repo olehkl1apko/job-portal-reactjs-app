d09cbab488c5b24535482742644c964e53cea5c1
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-03 15:47:55 +0300

    ADD: notifications actions & reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import alertReducer from "./alertSlice";
+import notificationsReducer from "./notificationsSlice";
 
 const store = configureStore({
   reducer: {
     alert: alertReducer,
+    notifications: notificationsReducer,
   },
 });
 
