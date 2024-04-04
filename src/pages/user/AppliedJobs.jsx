bdc05d0b6f00eafcad0fa31c8f48078f192f652c
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-04 13:44:15 +0300

    ADD: applied jobs page

--- src/pages/user/AppliedJobs.jsx
+++ src/pages/user/AppliedJobs.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { message, Table } from "antd";
 
-const AppliedJobs = () => {
-  return <div>AppliedJobs</div>;
-};
+import { PageTitle } from "../../components";
+import { HideLoading, ShowLoading } from "../../redux/alertSlice";
+import { getApplicationsByUserId } from "../../apis/jobs";
+
+function AppliedJobs() {
+  const dispatch = useDispatch();
+  const [data, setData] = useState([]);
+
+  const columns = [
+    {
+      title: "Job",
+      dataIndex: "jobTitle",
+    },
+    {
+      title: "Company",
+      dataIndex: "company",
+    },
+    {
+      title: "Applied On",
+      dataIndex: "appliedOn",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        dispatch(ShowLoading());
+        const user = JSON.parse(localStorage.getItem("user"));
+        const response = await getApplicationsByUserId(user.id);
+        if (response.success) {
+          setData(response.data);
+        }
+        dispatch(HideLoading());
+      } catch (error) {
+        dispatch(HideLoading());
+        message.error(error.message);
+      }
+    };
+
+    getData();
+  }, [dispatch]);
+
+  return (
+    <div>
+      <div className="d-flex justify-content-between">
+        <PageTitle title="Applied Jobs" />
+      </div>
+
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
+}
 
 export default AppliedJobs;
